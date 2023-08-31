3d67ae19fb7a6a38743b289256e93bf7c4a3c865
Iván Casas <73560533+ivanool@users.noreply.github.com>
2023-08-31 12:43:27 -0600

    Create Administración.java

---
+++ Administración.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Administracion {
+    private static List <Empleado> lista_empleados;
+    public Administracion(){
+        lista_empleados = new ArrayList<>();
+        
+
+    }
+
+    public void registrar(){
+
+    }
+    
+    public void listarinfo(){
+
+    }
+
+}
+
+//https://prod.liveshare.vsengsaas.visualstudio.com/join?46B7E9577057F0E4A1A4CE937887BC7FA9CD
