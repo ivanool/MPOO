b031c6f1e631dd955a09654cc5963de901e76500
Iván Casas <73560533+ivanool@users.noreply.github.com>
2023-09-02 19:25:34 +0000

    menssage 1

--- Administración.java
+++ Administración.java
@@ -1,22 +1,51 @@
-import java.util.List;
-import java.util.ArrayList;
-
 public class Administracion {
-    private static List <Empleado> lista_empleados;
-    public Administracion(){
+    private static List<Empleado> lista_empleados;
+
+    public Administracion() {
         lista_empleados = new ArrayList<>();
-        
-
     }
 
-    public void registrar(){
-
-    }
-    
-    public void listarinfo(){
-
+    public void registrar() {
+        Empleado empleado = new Empleado();
+        empleado.capturar();
+        lista_empleados.add(empleado);
     }
 
+    public void listarinfo() {
+        for (Empleado empleado : lista_empleados) {
+            empleado.listar();
+        }
+    }
+
+    public static void main(String[] args) {
+        Administracion admin = new Administracion();
+        int opcion;
+
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Registrar empleado");
+            System.out.println("2. Listar empleados");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+            sc.nextLine(); // Limpia el buffer de entrada
+
+            switch (opcion) {
+                case 1:
+                    admin.registrar();
+                    break;
+                case 2:
+                    admin.listarinfo();
+                    break;
+                case 3:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcion != 3);
+    }
 }
+
 
 //https://prod.liveshare.vsengsaas.visualstudio.com/join?46B7E9577057F0E4A1A4CE937887BC7FA9CD
--- Empleados.java
+++ Empleados.java
@@ -1,55 +1,58 @@
 import java.util.Scanner;
 
 public class Empleado {
-    //ATRIBUTOS
+    // ATRIBUTOS
     private int nEmpleado;
     private String nombre;
     private float sueldo;
     private Scanner sc;
 
-    //Metodos
+    // CONSTRUCTOR
+    public Empleado() {
+        sc = new Scanner(System.in);
+    }
 
-  
-
-    public void capturar(){
+    // Métodos de captura
+    public void capturar() {
         System.out.println("Ingresa el número del empleado:");
         nEmpleado = sc.nextInt();
-        sc.nextLine();
+        sc.nextLine(); // Limpia el buffer de entrada
         System.out.println("Ingresa el nombre del empleado:");
         nombre = sc.nextLine();
         System.out.println("Ingresa el sueldo del empleado:");
         sueldo = sc.nextFloat();
     }
 
-    public void listar(){
+    // Método de presentación en pantalla
+    public void listar() {
         System.out.println("Los datos del empleado son los siguientes:");
-        System.out.println("Nombre: " + nombre + "Numero: " + nEmpleado + "Sueldo: "+ sueldo);
+        System.out.println("Nombre: " + nombre + ", Numero: " + nEmpleado + ", Sueldo: " + sueldo);
     }
 
-    public void setnEmpleado(int nEmpleado){
+    // Métodos setter para actualizar atributos
+    public void setNEmpleado(int nEmpleado) {
         this.nEmpleado = nEmpleado;
     }
 
-    public void setSueldo(Float sueldo){
+    public void setSueldo(float sueldo) {
         this.sueldo = sueldo;
     }
-    
-    public void setNombre(String nombre){
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
-    
-    
-    public float getSueldo(){
+
+    // Métodos getter para obtener atributos
+    public float getSueldo() {
         return sueldo;
     }
-    
-    public int getnEmpleado(){
+
+    public int getNEmpleado() {
         return nEmpleado;
     }
 
-    public String getnombre(){
+    public String getNombre() {
         return nombre;
     }
-
 }
+
